cd971628a824554a0186bf7c79ccc18edbea5e08
Iain Shorter <iainshorter@gmail.com>
2016-04-28 18:38:11 +0100

    New

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,3 +1,11 @@
+/*
+ *  #     #
+ *  #     #
+ *   #   #
+ *    # #
+ *     #
+ */
+
 class Vector {
 	constructor (x, y) {
 		this.set(x, y);
@@ -29,8 +37,22 @@ class Vector {
 		this.y *= s;
 		return this;
 	}
+	length () {
+		return Math.sqrt(this.x * this.x + this.y * this.y);
+	}
+	distanceTo ({x, y}) {
+		const DX = this.x - x;
+		const DY = this.y - y;
+		return Math.sqrt(DX * DX + DY * DY);
+	}
 }
-
+/*
+ *   #####
+ *  #     #
+ *	#
+ *	#     #
+ * 	 #####
+ */
 class Canvas {
 	constructor () {
 		this.element = document.createElement('canvas');
@@ -66,9 +88,36 @@ class Canvas {
 						break;
 					}
 				}
-
 			}
 		}
+		
+		requestAnimationFrame(DRAW);
+		this.element.addEventListener('mousedown', MOUSE_DOWN);
+		this.element.addEventListener('mousemove', MOUSE_MOVE);
+		this.element.addEventListener('mouseup', MOUSE_UP);
+	}
+	selectLayer (L) {
+		if (this.selectedLayer)
+			this.deselectLayer();
+		this.selectedLayer = L;
+		L.select();
+	}
+	deselectLayer (L) {
+		L = L || this.selectedLayer;
+		if (L === this.selectedLayer)
+		{
+			this.selectedLayer = null;
+			L.deselect();
+		}
+	}
+	addLayer (L) {
+		this.layers.push(L);
+	}
+	removeLayer (L) {
+		const i = this.layes.indexOf(L);
+		this.layers.splice(i, 1);
+		if (L === this.selectedLayer)
+			this.deselectLayer();
 	}
 	clear () {
 		this.context.clearRect(0, 0, this.width, this.height);
@@ -99,6 +148,10 @@ class Canvas {
 		C.fillRect(x - R, y - R, size, size);
 		return this;
 	}
+	image (img, {x, y}, D) {
+		this.context.drawImage(img, x, y, D.x, D.y);
+		return this;
+	}
 	set width (v) {
 		return this.element.width = v;
 	}
@@ -112,7 +165,13 @@ class Canvas {
 		return this.element.height;
 	}
 }
-
+/*
+ *  #
+ *  #
+ *	#
+ *	#
+ * 	#######
+ */
 class Layer {
 	constructor () {
 		this.visible = true;
@@ -122,5 +181,95 @@ class Layer {
 	}
 	draw (C) {
 		
+	}
+	select () {
+		
+	}
+	deselect () {
+		
+	}
+}
+
+class Polygon extends Layer {
+	constructor () {
+		super();
+		this.points = [];
+		this.fillStyle = null;
+		this.lineStyle = null;
+	}
+	inside (V) {
+		return Collision.pointInPolygon(V, this.points);
+	}
+	draw (C) {
+		if (this.fillStyle)
+			C.fill(this.points, this.fillStyle);
+		if (this.strokeStyle)
+			C.fill(this.points, this.strokeStyle);
+	}
+}
+
+class Polyline extends Layer {
+	constructor () {
+		super();
+		this.points = [];
+		this.strokeStyle = null
+	}
+	inside (V) {
+		let A;
+		for (let B of this.points)
+		{
+			if (A) {
+				if (Collision.pointNearLine(V, A, B, 5))
+					return true;
+			}
+			A = B
+		}
+		return false;
+	}
+	draw (C) {
+		if (this.strokeStyle)
+			C.stroke(this.points, this.strokeStyle);
+	}
+}
+
+class Image extends Layer {
+	constructor (image) {
+		super();
+		this.position = new Vector();
+		this.size = new Vector();
+		this.image = 
+	}
+	inside (V) {
+		return Collision.pointInBox(V, this.position, this.size);
+	}
+	draw (C) {
+		C.image
+	}
+}
+
+class Collision {
+	constructor () {
+		
+	}
+	static pointInPolygon ({x, y}, polygon) {
+	    let inside = false;
+		let intersect;
+		let A;
+		for (let B of polygon) {
+			if (A)
+			{
+				intersect = ((A.y > y) != (B.y > y))
+					&& (x < (B.x - A.x) * (y - A.y) / (B.y - A.y) + A.x);
+				intersect && (inside = !inside);
+			}
+			A = B;
+		}
+	    return inside;
+	}
+	static pointNearLine ({x, y}, A, B, D) {
+		
+	}
+	static pointInBox ({x, y}, P, D) {
+		return (P.x <= x) && (P.x + D.x >= x) && (P.y <= y) && (P.y + D.y >= y);
 	}
 }
